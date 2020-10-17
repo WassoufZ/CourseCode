bcb842c002367c59170ae218f5d57d58899a5fc0
YahyaStihi18 <stihiyahya2016@gmail.com>
2020-10-18 00:41:20 +0100

    Update settings.py

--- FYK/settings.py
+++ FYK/settings.py
@@ -206,9 +206,9 @@ MEDIA_URL ='/media/'
 
 
 #====== aws3 settings===============
-AWS_ACCESS_KEY_ID = 'AKIA272UMKGOOGTJXR74'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'new-test-django2'
+AWS_ACCESS_KEY_ID = '***********'
+AWS_SECRET_ACCESS_KEY = '****************************************'
+AWS_STORAGE_BUCKET_NAME = '***************'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
